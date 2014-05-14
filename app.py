07f56aacc404843d824831cd341e74eecea1e141
Robert Simpson <ttfnrob@gmail.com>
2014-05-14 21:04:22 +0100

    Timeouts, loops and myCron

--- app.py
+++ app.py
@@ -2,14 +2,22 @@ import os
 from flask import Flask, render_template, send_from_directory
 import urllib2
 import json
-from tracker import trackBuses
+from tracker import *
 from bs4 import BeautifulSoup
+from nocache import nocache
+import time, threading
 
 # initialization
 app = Flask(__name__)
 app.config.update(
     DEBUG = True
 )
+
+def mycron(sid):
+    print "Creating new JSON files"
+    print createJSON(sid,"1")
+    print createJSON(sid,"2")
+    threading.Timer(60, mycron(sid)).start()
 
 # controllers
 @app.route('/favicon.ico')
@@ -21,10 +29,12 @@ def page_not_found(e):
     return render_template('404.html'), 404
 
 @app.route("/")
+@nocache
 def index():
     return render_template('index.html')
 
 @app.route("/process")
+@nocache
 def process():
     buses = trackBuses("S1","1")
     with open('data/S1.json', 'w') as fp:
@@ -33,5 +43,7 @@ def process():
 
 # launch
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 4000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
+    mycron("S1")
+    mycron("S2")
--- static/js/mapper.js
+++ static/js/mapper.js
@@ -1,16 +1,25 @@
-var map;
+function deleteMarkers() {
+  
+}
+
+function loadData() {
+  // Clear markers and load a GeoJSON
+  var ts = Math.floor(Date.now()/1000);
+  // console.log("Reloading geoJSON with ts "+ts);
+  deleteMarkers();
+  map.data.loadGeoJson('static/data/S1_1.json?ts='+ts);
+  map.data.loadGeoJson('static/data/S1_2.json?ts='+ts);
+  map.data.loadGeoJson('static/data/S2_1.json?ts='+ts);
+  map.data.loadGeoJson('static/data/S2_2.json?ts='+ts);
+  setTimeout(loadData,5000);
+}
+
 function initialize() {
   // Create a simple map.
   map = new google.maps.Map(document.getElementById('map-canvas'), {
     zoom: 11,
     center: {lat: 51.76, lng: -1.36}
   });
-
-  // Load a GeoJSON
-  map.data.loadGeoJson('static/data/S1_1.json');
-  map.data.loadGeoJson('static/data/S1_2.json');
-  map.data.loadGeoJson('static/data/S2_1.json');
-  map.data.loadGeoJson('static/data/S2_2.json');
 
   var imgS1_1 = 'http://gmaps-samples.googlecode.com/svn/trunk/markers/green/marker1.png';
   var imgS1_2 = 'http://gmaps-samples.googlecode.com/svn/trunk/markers/red/marker1.png';
@@ -19,7 +28,6 @@ function initialize() {
 
   map.data.setStyle(function(feature) {
     var service = feature.getProperty('service');
-    console.log(service);
     if (service==="S1") {
       return ({ icon: imgS1_1 });
     }
@@ -32,6 +40,10 @@ function initialize() {
     console.log(event.feature);
   });
 
+  loadData();
+
 }
 
+var map;
+var markers = [];
 google.maps.event.addDomListener(window, 'load', initialize);
